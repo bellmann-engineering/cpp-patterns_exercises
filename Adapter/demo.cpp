ea6c13e01b9529600063654eeb005943ff071ccd
bellmann-engineering <57789413+bellmann-engineering@users.noreply.github.com>
2023-02-22 11:42:42 +0100

    Create demo.cpp

---
+++ Adapter/demo.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+// a class containing only pure virtual methods ≙ interface
+class ThermoInterface
+{
+    public:
+        virtual double getTempC() = 0;
+};
+
+// Lecacy component
+class CelciusThermo : public ThermoInterface
+{
+    public:
+        double getTempC() override
+        {
+            return -20;
+        }
+    
+};
+
+class FahrenheitThermo
+{
+    public:
+        double getFahrenheitTemperature() 
+        {
+            return -4; // -4 °F should be -20° C
+        }
+    
+};
+
+int main()
+{
+    // TODO: Implement ThermoAdapter
+    ThermoInterface* ti = new ThermoAdapter();
+    std::cout << ti->getTempC() << std::endl;
+
+    if(ti != nullptr)
+       delete ti;
+}
